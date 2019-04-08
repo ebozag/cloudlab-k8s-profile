f5d1660d5c2f6f704d28b59baa476046374fa2cf
Edwin Boza <eboza@espol.edu.ec>
2019-04-08 22:16:12 +0000

    Fix installed version of cni

--- ubuntu18/slave.sh
+++ ubuntu18/slave.sh
@@ -32,7 +32,7 @@ version=$(echo $(echo $K8SVERSION |sed 's/v//')-00)
 sudo apt install -y docker.io=18.06.1-0ubuntu1.2~18.04.1
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo apt-get -y kubernetes-cni=0.6.0-00 golang-go jq
+sudo apt-get install -y kubernetes-cni=0.6.0-00 golang-go jq
 sudo docker version
 sudo swapoff -a
 
